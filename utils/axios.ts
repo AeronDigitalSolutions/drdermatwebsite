985500e3e31325c7ac7e7034a38cb26d07ece928
Ankit <ankitsingh110198@gmail.com>
2025-10-13 11:40:54 +0530

    test

--- utils/axios.ts
+++ utils/axios.ts
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL || "/api",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
-
-
 export default API;
+
